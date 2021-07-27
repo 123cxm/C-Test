ed450eee08123bae74c0b603f753877d0966d1a7
chenxuemei <chenxuemei1@bdstar.com>
2021-07-27 18:22:13 +0800

    构造函数的定义

---
+++ 构造函数/构造函数/构造函数.cpp
@@ -0,0 +1,30 @@
+﻿// 构造函数.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
+#include <iostream>
+using namespace std;
+class person
+{
+public :
+	person() //构造函数
+	{
+		cout << "person 构造函数的调用" << endl;
+	}
+	~ person() //析构函数
+	{
+		cout << "person 析构函数的调用" << endl;
+	}
+};
+void test01()
+{
+	person p;
+}
+int main()
+{
+	test01();
+
+	person p;
+	system("pause");
+	return 0;
+}
+
